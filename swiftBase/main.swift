9fc8160a6a1d5a9261251cd46745a11ddd547f46
Evgenii Elchev <yolchev@gmail.com>
2022-06-06 14:36:05 +0700

    print example

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,1 +1,10 @@
-dataTypeTest()
+print(1)
+print(2.4)
+print(Float(3.14))
+print("Привет мир")
+print(Character("f"))
+print(true)
+print(1, 2.4, Float(3.14), "Привет мир", Character("f"), true)
+
+
+
