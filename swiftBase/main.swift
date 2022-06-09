2b1e5941d68c076db9e1902b44fef7f87b1b2439
Evgenii Elchev <yolchev@gmail.com>
2022-06-09 19:20:47 +0700

    add examples form video

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,10 +1,109 @@
-print(1)
-print(2.4)
-print(Float(3.14))
-print("Привет мир")
-print(Character("f"))
-print(true)
-print(1, 2.4, Float(3.14), "Привет мир", Character("f"), true)
+/*
+let a: Int // let %имя константы%: %тип константы%
+let b: Double
+let c: Float
+let d: String
+let e: Character
+let f: Bool
+*/
+
+/*
+ let user: Int      // хороше имя
+ let b: Int         // плохое имя
+ let 1user: Int     // некоректное имя, приложение не скомпилиируется
+ let User: Int      // плохое имя
+ let myuser: Int    // плохое имя, слова сливаются
+ let my_user: Int   // плохое имя, в iOS разрабоке обычно не используют нижнее подчеркивание для разделения слов
+ let myUser: Int    // хороше имя
+ */
+
+/*
+ let user: Int
+ print(user)
+ */
+
+/*
+ let user: Int
+ user = 7
+ print(user)
+ */
+
+/*
+ let a = 1
+ a = 2
+ */
+
+/*
+ let a: Int = 1
+ let b: Double = 2.3
+ let c: Float = 3.14
+ let d: String = "Привет мир!"
+ let e: Character = "d"
+ let f: Bool = true
+ */
+
+/*
+ let a: Int = 1
+ let b: Double = 1
+ let c: Float = 1
+ let d: String = 1
+ let e: Character = 1
+ let f: Bool = 1
+ let x: Int = 1.0
+ let v: Int = "привет"
+ */
+
+/*
+ let a: Int = m
+ let a: Int = m
+ let b: Double = m
+ let c: Float = m
+ let d: String = m
+ let e: Character = m
+ let f: Bool = m
+ */
+
+/*
+ let a = 1               // Int
+ let b = 2.3             // Double
+ let c: Float = 3.14     // Float
+ let d = "Привет мир!"   // String
+ let e: Character = "d"  // Character
+ let f = true            // Bool
+ */
 
 
+/*
+ let a0 = 1
+ let a = a0               // Int
+ let b0 = 2.3
+ let b = b0              // Double
+ let c0: Float = 3.14
+ let c = c0              // Float
+ let d0 = "Привет мир!"
+ let d = d0              // String
+ let e0: Character = "d"
+ let e = e0              // Character
+ let f0 = true
+ let f = f0              // Bool
+ */
 
+/*
+ var a: Int // var %имя переменной%: %тип переменной%
+ a = 1
+ var b: Double = 2.4
+ var c: Float = 3.14
+ var d = "Привет мир"
+ var e: Character = "e"
+ var f = true
+ */
+
+/*
+ let a = 1
+ a = 2
+ 
+ var b = 1
+ b = 2
+ b = a
+ b = 3
+ */
