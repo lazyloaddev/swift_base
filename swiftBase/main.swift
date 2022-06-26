7fdeb7a0018bc31f018400c481910b3da7c90eeb
Evgenii Elchev <yolchev@gmail.com>
2022-06-26 15:56:40 +0700

    optional

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,102 +1,125 @@
-/*
- print("Привет, незнакомец!") // выводим в консоль сообщение
- if age < 20 { // результат сравнения будет true
-     print("К сожалению ты слишком молод.") // этот код выполнится
- }
- */
-/*
- let age = 42 // объявляем константу
+let nameFromuserInput = readLine()
 
- print("Привет, незнакомец!") // выводим в консоль сообщение
- if age < 20 { // результат сравнения будет false
-     print("К сожалению ты слишком молод.") // этот код не выполнится
- }
- */
-/*
- let age = 42
-
- print("Привет, незнакомец!")
- if age < 10 {
-     print("К сожалению ты слишком молод.")
- } else {
-     print("К сожалению ты слишком стар.")
- }
- */
-/*
- let age = 25
-
- print("Привет, незнакомец!")
- if age < 20 {
-     print("К сожалению ты слишком молод.")
- } else if age >= 20 && age <= 25 {
-     print("Отличный возраст!")
- } else {
-     print("К сожалению ты слишком стар.")
- }
- */
-/*
- let age = 25
-
- print("Привет, незнакомец!")
- if age < 30 {
-     print("К сожалению ты слишком молод.")
- } else if age >= 20 && age <= 25 {
-     print("Отличный возраст!")
- } else {
-     print("К сожалению ты слишком стар.")
- }
- */
-/*
- let age = 42
- let name = "Иван"
-
- print("Привет, незнакомец!")
- if age < 30 {
-     print("К сожалению ты слишком молод.")
- } else if age >= 20 && age <= 25 {
-     print("Отличный возраст!")
- } else if name == "Иван" {
-     print("Ты тот кого я ищу!")
- } else {
-     print("К сожалению ты слишком стар.")
- }
- */
-/*
- let age = 42
- let name = "Иван"
+if let nameFromuserInput = nameFromuserInput {
+    print(nameFromuserInput)
+} else {
+    print("При извдечении имени пользователя обнаружилось nill")
+}
 
 
- switch age {
- case ..<20: print("Привет, незнакомец!")
- case 20...25: print("Отличный возраст!")
- case 26...: print("К сожалению ты слишком стар.")
- default: print("Кто ты")
- }
- */
 /*
- let name = "Иван"
+let a: Int = 1
+let b: Int? = 1
+let c: Int? = nil
+ 
 
- switch name {
- case "Егор": print("Привет, незнакомец!")
- case "Василий": print("Мы не знакомы")
- case "Иван": print("Ты тот кто мне нужен")
- default: print("Кто ты")
- }
+print(a + (b ?? 0))
+print(a + (c ?? 0))
  */
+
 /*
- let age = 42
+let a: Int = 1
+let b: Int! = 1
+let c: Int! = nil
 
- let message: String
- if age < 20 {
-     message = "слишком молод"
- } else {
-     message = "слишком стар"
- }
- print(message)
- */
+print(a + b)
+print(a + c)
+*/
 /*
- let age = 42
+let a: Int = 1
+let b: Int? = 1
+let c: Int? = nil
 
- let message = age < 20 ? "слишком молод" : "слишком стар"
- print(message)
- */
+print(a + b!)
+print(a + c!)
+*/
+/*
+let a: Int = 1
+let b: Int? = 1
+let c: Int? = nil
+
+if
+    let b = b,
+    let c = c
+{
+    print(b + c)
+} else {
+    print("В b или c нет значения")
+}
+*/
+/*
+let a: Int = 1
+let b: Int? = 1
+let c: Int? = nil
+
+if let b = b {
+    // b не опциональная
+    print(a + b)
+} else {
+    print("В b нет значения")
+}
+// b опциональная
+*/
+
+/*
+let a: Int = 1
+let b: Int? = 1
+let c: Int? = nil
+
+if let usualB = b {
+    print(a + usualB)
+} else {
+    print("В b нет значения")
+}
+
+if let usualC = c {
+    print(a + usualC)
+} else {
+    print("В c нет значения")
+}
+*/
+
+/*
+// 1
+let a: Int? = 1
+let b: Int? = 5
+let c: Int = a
+
+// 2
+
+let result1 = a + a
+let result2 = a + c
+
+// 3
+
+print(a)
+*/
+
+/*
+// 1
+let a: Int = 1
+let b: Int? = 1
+
+// 2
+var c: Int? = 1
+c = 2
+c = nil
+c = 5
+c = 1
+
+// 3
+
+c = a
+let d: Int? = a
+
+// 4
+
+print(a == b)
+print(b == c)
+*/
+
+/*
+let a: Int? = nil
+let b: String? = nil
+let c: Double? = nil
+*/
