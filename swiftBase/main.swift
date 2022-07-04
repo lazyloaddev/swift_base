45913833103fe8da0905024bf35bcc2a460c4b22
Evgenii Elchev <yolchev@gmail.com>
2022-07-04 22:17:00 +0700

    functions

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,54 +1,140 @@
-let names: [String]? = nil
+let s = sum(1, 2)
+func sum(_ a: Int, _ b: Int) -> Int {
+    a + b
+}
 /*
-let names = [] // так нельзя, компилятор не понимает какой тип будет храниться
-// нужно
-let names = [String]()
-// или
-let names:[String] = []
+func isTwo(_ a: Int) -> String {
+    if a == 2 {
+        return "Это двойка"
+    } else {
+        return "Это не двойка"
+    }
+}
+let a = isTwo(3)
+*/
+/*
+func sum() -> Int {
+    print("Раз")
+    return 10
+    print("Два")
+}
+ 
+let a = sum()
+ */
+/* В консоли мы увидим
+ Раз
+ Program ended with exit code: 0
  */
 /*
-let names = ["Иван", "Петр", "Виктория"]
-*/
-/*
-let names: [String] = ["Иван", "Петр", "Виктория"]
+func getUserFullName() -> String {
+    print("Введите ваше имя:")
+    let name = readLine() ?? ""
+    print("Выедите вашу фамилию:")
+    let surname = readLine() ?? ""
+    return name + "" + surname
+}
+let userFullName = getUserFullName()
+print(userFullName)
  */
 /*
-let names: Array<String> = Array<String>(arrayLiteral: "Иван", "Петр", "Виктория")
- *\
+ func sum(_ a: Int, _ b: Int) {
+     print(a + b)
+ }
+ func sum(_ a: Int, _ b: Int) -> Int {
+     return a + b
+ }
+ func sum(_ a: Double, _ b: Double) {
+     print(a + b)
+ }
+ func sum(first a: Int, _ b: Int) {
+     print(a + b)
+ }
+ */
 /*
-let age = 25
-let name = "Иван"
-let salary: Array<Int> = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
-let childenNames: Array<String> = ["Егор", "Екатерина", "Василиса"]
+func sum(_ a: Int, _ b: Int) {
+    print(a + b)
+}
+sum(1, 1)
 */
 /*
-let age = 25
-let name = "Иван"
-let salary = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
-let childenNames = ["Егор", "Екатерина", "Василиса"]
+func sum(_ a: Int, _ b: Int) -> Void {
+    print(a + b)
+}
+sum(1, 1)
 */
 /*
-let age = 25
-let name = "Иван"
-let salary1 = 50001
-let salary2 = 47002
-let salary3 = 53003
-let salary4 = 51004
-let salary5 = 52005
-let salary6 = 55006
-let salary7 = 59007
-let salary8 = 56008
-let salary9 = 58009
-let salary10 = 57010
-let salary11 = 50011
-let salary12 = 50012
-let child1Name = "Егор"
-let child2Name = "Екатерина"
-let child3Name = "Василиса"
+func sum(_ a: Int, _ b: Int) -> String {
+    return String(a + b)
+}
+let a: String = sum(1, 1)
+print(a)
 */
 /*
-let age = 25
-let name = "Иван"
-let salary = 50000
-let childName = "Егор"
+func sum(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+let a: Int = sum(1, 1)
+print(a)
+*/
+/*
+func sum(
+    _ first: Int,
+    second: Int,
+    thridNumber thrid: Int
+) -> Int {
+    return first + second + thrid
+}
+let a = sum(1, second: 2, thridNumber: 3) // 6
+ */
+/*
+ func sum(
+     _ first: Int,
+     _ second: Int,
+     _ thrid: Int
+ ) -> Int {
+     return first + second + thrid
+ }
+ let a = sum(1, 2, 3) // 6
+ */
+/*
+ func sum(
+     first: Int,
+     second: Int,
+     thrid: Int
+ ) -> Int {
+     return first + second + thrid
+ }
+ let a = sum(first: 1, second: 2, thrid: 3) // 6
+ */
+/*
+func sum(
+    firstNumber first: Int,
+    secondNumber second: Int,
+    thridNumber thrid: Int
+) -> Int {
+    return first + second + thrid
+}
+*/
+/*
+let a = sum(firstNumber: 4, secondNumber: 3, thridNumber: 2) // 9
+ */
+/*
+func sum() -> Int {
+    return 5 + 5
+}
+let a = sum() // 10
+let b = sum() // 10
+ */
+/*
+func sum(firstNumber first: Int, secondNumber second: Int) -> Int {
+    return first + second
+}
+let a = sum(firstNumber: 5, secondNumber: 5)
+let b = sum(firstNumber: 2, secondNumber: 9)
+print(a, b)
+ */
+/* В консоле мы увидим
+ 10 11
+ Program ended with exit code: 0
+ 
 */
