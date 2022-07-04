5d70901387d2c9c72c19f37613717a7f304c50a4
Evgenii Elchev <yolchev@gmail.com>
2022-07-04 15:21:35 +0700

    arrays

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,33 +1,132 @@
-print("Добро пожаловать в программу калькулятор.")
+let a: [Any] = [1, 2, "привет", 3]
+print(a)
 
-print("Выберете операцию: +, -, * или /")
-let operation = readLine() ?? ""
+/*
+var a = [123, 3, 4, 5, 6]
+a.removeAll()
+print(a)
+*/
 
-print("Введите целое число:")
-let firstNumber = readLine() ?? ""
+/*
+ var a = [123, 3, 4, 5, 6]
+ a.remove(at: 0)
+ print(a)
+ a.remove(at: 2)
+ print(a)
+ */
+/*
+ [0, 3, 4, 5, 6]
+ Program ended with exit code: 0
+ */
+/*
+ var a = [123, 3, 4, 5, 6]
+ a.insert(0, at: 10)
+ print(a)
+ */
+/*
+var a = [123, 3, 4, 5, 6]
+a.insert(0, at: 0)
+print(a)
+a.insert(0, at: 3)
+print(a)
+ */
+/*
+ var a = [123, 3, 4, 5, 6]
+ a.append(0)
+ print(a)
+ */
 
-print("Введите второе число:")
-let secondNumber = readLine() ?? ""
-
-print("Идет вычисление примера: " + firstNumber + " " + operation + " " + secondNumber)
-
-if let firstNumber = Int(firstNumber) {
-    if let secondNumber = Int(secondNumber) {
-        switch operation {
-        case "+": print("Результат: " + String(firstNumber + secondNumber))
-        case "-": print("Результат: " + String(firstNumber - secondNumber))
-        case "*": print("Результат: " + String(firstNumber * secondNumber))
-        case "/":
-            if secondNumber != 0 {
-                print("Результат: " + String(firstNumber / secondNumber))
-            } else {
-                print("Деление на 0 является недопустимой операцией")
-            }
-        default: print("Вы ввели не верную операцию.")
-        }
-    } else {
-        print("Вы ввели не верное второе число")
-    }
+/*
+let a = [123, 3, 4, 5, 6]
+if a.count > 5 {
+    print(a[5])
 } else {
-    print("Вы ввели не верное первое число")
+    print("нет такого значения")
 }
+ */
+/*
+ let a = [123, 3, 4, 5, 6]
+ print(a[5])
+ */
+/*
+ let a = [123, 3, 4, 5, 6]
+ */
+/* В консоле мы увидим
+ 123 3 4 5 6
+ Program ended with exit code: 0
+ */
+/*
+let a = [
+    123,    // 0
+    3,      // 1
+    4,      // 2
+    5,      // 3
+    6       // 4
+]
+ */
+/*
+var b = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+ */
+/*
+ var a = 2
+ a = 10
+ var b = [1, 2, 3, 4, 5]
+ b = [6, 7, 8, 9, 10]
+ */
+/*
+let names: [String]? = nil
+ */
+/*
+let names = [] // так нельзя, компилятор не понимает какой тип будет храниться
+// нужно
+let names = [String]()
+// или
+let names:[String] = []
+ */
+/*
+let names = ["Иван", "Петр", "Виктория"]
+*/
+/*
+let names: [String] = ["Иван", "Петр", "Виктория"]
+ */
+/*
+let names: Array<String> = Array<String>(arrayLiteral: "Иван", "Петр", "Виктория")
+ */
+/*
+let age = 25
+let name = "Иван"
+let salary: Array<Int> = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
+let childenNames: Array<String> = ["Егор", "Екатерина", "Василиса"]
+*/
+/*
+let age = 25
+let name = "Иван"
+let salary = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
+let childenNames = ["Егор", "Екатерина", "Василиса"]
+*/
+/*
+let age = 25
+let name = "Иван"
+let salary1 = 50001
+let salary2 = 47002
+let salary3 = 53003
+let salary4 = 51004
+let salary5 = 52005
+let salary6 = 55006
+let salary7 = 59007
+let salary8 = 56008
+let salary9 = 58009
+let salary10 = 57010
+let salary11 = 50011
+let salary12 = 50012
+let child1Name = "Егор"
+let child2Name = "Екатерина"
+let child3Name = "Василиса"
+*/
+/*
+let age = 25
+let name = "Иван"
+let salary = 50000
+let childName = "Егор"
+*/
