798da81297eef9301e76a5dc0da57619e5a4ee85
Evgenii Elchev <yolchev@gmail.com>
2022-07-04 15:21:35 +0700

    arrays

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,33 +1,54 @@
-print("Добро пожаловать в программу калькулятор.")
-
-print("Выберете операцию: +, -, * или /")
-let operation = readLine() ?? ""
-
-print("Введите целое число:")
-let firstNumber = readLine() ?? ""
-
-print("Введите второе число:")
-let secondNumber = readLine() ?? ""
-
-print("Идет вычисление примера: " + firstNumber + " " + operation + " " + secondNumber)
-
-if let firstNumber = Int(firstNumber) {
-    if let secondNumber = Int(secondNumber) {
-        switch operation {
-        case "+": print("Результат: " + String(firstNumber + secondNumber))
-        case "-": print("Результат: " + String(firstNumber - secondNumber))
-        case "*": print("Результат: " + String(firstNumber * secondNumber))
-        case "/":
-            if secondNumber != 0 {
-                print("Результат: " + String(firstNumber / secondNumber))
-            } else {
-                print("Деление на 0 является недопустимой операцией")
-            }
-        default: print("Вы ввели не верную операцию.")
-        }
-    } else {
-        print("Вы ввели не верное второе число")
-    }
-} else {
-    print("Вы ввели не верное первое число")
-}
+let names: [String]? = nil
+/*
+let names = [] // так нельзя, компилятор не понимает какой тип будет храниться
+// нужно
+let names = [String]()
+// или
+let names:[String] = []
+ */
+/*
+let names = ["Иван", "Петр", "Виктория"]
+*/
+/*
+let names: [String] = ["Иван", "Петр", "Виктория"]
+ */
+/*
+let names: Array<String> = Array<String>(arrayLiteral: "Иван", "Петр", "Виктория")
+ *\
+/*
+let age = 25
+let name = "Иван"
+let salary: Array<Int> = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
+let childenNames: Array<String> = ["Егор", "Екатерина", "Василиса"]
+*/
+/*
+let age = 25
+let name = "Иван"
+let salary = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
+let childenNames = ["Егор", "Екатерина", "Василиса"]
+*/
+/*
+let age = 25
+let name = "Иван"
+let salary1 = 50001
+let salary2 = 47002
+let salary3 = 53003
+let salary4 = 51004
+let salary5 = 52005
+let salary6 = 55006
+let salary7 = 59007
+let salary8 = 56008
+let salary9 = 58009
+let salary10 = 57010
+let salary11 = 50011
+let salary12 = 50012
+let child1Name = "Егор"
+let child2Name = "Екатерина"
+let child3Name = "Василиса"
+*/
+/*
+let age = 25
+let name = "Иван"
+let salary = 50000
+let childName = "Егор"
+*/
