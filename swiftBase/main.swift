2879ee2ddd55ae70d87d3bb98ec2a34b2cfee95b
Evgenii Elchev <yolchev@gmail.com>
2022-07-07 11:11:46 +0700

    scoup

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,140 +1,62 @@
-let s = sum(1, 2)
-func sum(_ a: Int, _ b: Int) -> Int {
-    a + b
-}
-/*
-func isTwo(_ a: Int) -> String {
-    if a == 2 {
-        return "Это двойка"
-    } else {
-        return "Это не двойка"
-    }
-}
-let a = isTwo(3)
-*/
-/*
-func sum() -> Int {
-    print("Раз")
-    return 10
-    print("Два")
-}
- 
-let a = sum()
- */
-/* В консоли мы увидим
- Раз
- Program ended with exit code: 0
- */
-/*
-func getUserFullName() -> String {
-    print("Введите ваше имя:")
-    let name = readLine() ?? ""
-    print("Выедите вашу фамилию:")
-    let surname = readLine() ?? ""
-    return name + "" + surname
-}
-let userFullName = getUserFullName()
-print(userFullName)
- */
-/*
- func sum(_ a: Int, _ b: Int) {
-     print(a + b)
- }
- func sum(_ a: Int, _ b: Int) -> Int {
-     return a + b
- }
- func sum(_ a: Double, _ b: Double) {
-     print(a + b)
- }
- func sum(first a: Int, _ b: Int) {
-     print(a + b)
- }
- */
-/*
-func sum(_ a: Int, _ b: Int) {
-    print(a + b)
-}
-sum(1, 1)
-*/
-/*
-func sum(_ a: Int, _ b: Int) -> Void {
-    print(a + b)
-}
-sum(1, 1)
-*/
-/*
-func sum(_ a: Int, _ b: Int) -> String {
-    return String(a + b)
-}
-let a: String = sum(1, 1)
-print(a)
-*/
 /*
 func sum(_ a: Int, _ b: Int) -> Int {
-    return a + b
+    let a = 32
+    let result = a + b
+    return result
 }
-let a: Int = sum(1, 1)
-print(a)
-*/
-/*
-func sum(
-    _ first: Int,
-    second: Int,
-    thridNumber thrid: Int
-) -> Int {
-    return first + second + thrid
-}
-let a = sum(1, second: 2, thridNumber: 3) // 6
  */
 /*
- func sum(
-     _ first: Int,
-     _ second: Int,
-     _ thrid: Int
- ) -> Int {
-     return first + second + thrid
- }
- let a = sum(1, 2, 3) // 6
- */
-/*
- func sum(
-     first: Int,
-     second: Int,
-     thrid: Int
- ) -> Int {
-     return first + second + thrid
- }
- let a = sum(first: 1, second: 2, thrid: 3) // 6
- */
-/*
-func sum(
-    firstNumber first: Int,
-    secondNumber second: Int,
-    thridNumber thrid: Int
-) -> Int {
-    return first + second + thrid
+var c = 1
+if c == 1 {
+    c += 1
+    print(c)
+} else {
+    c += 1
+    print(c)
 }
-*/
-/*
-let a = sum(firstNumber: 4, secondNumber: 3, thridNumber: 2) // 9
- */
-/*
-func sum() -> Int {
-    return 5 + 5
+
+func sum(_ a: Int, _ b: Int) -> Int {
+    var c = 1
+    c += 1
+    let result = a + b + c
+    return result
 }
-let a = sum() // 10
-let b = sum() // 10
- */
-/*
-func sum(firstNumber first: Int, secondNumber second: Int) -> Int {
-    return first + second
-}
-let a = sum(firstNumber: 5, secondNumber: 5)
-let b = sum(firstNumber: 2, secondNumber: 9)
-print(a, b)
+
+print(sum(1, 2))
+print(sum(1, 2))
+print(sum(1, 2))
+print(sum(1, 2))
  */
 /* В консоле мы увидим
- 10 11
+ 2
+ 5
+ 5
+ 5
+ 5
  Program ended with exit code: 0
- 
-*/
+ */
+/*
+ let a = 5
+ let b = 4
+ let result = a + b
+
+ if a == b {
+     print("а == b")
+ } else {
+     print("а != b")
+ }
+
+
+ func sum(_ a: Int, _ b: Int) -> String {
+     let result = a + b
+     if result == 2 {
+         return "Двойка"
+     } else {
+         return "Не двойка"
+     }
+ }
+
+ let sumResult = sum(a, b)
+ print(sumResult)
+
+ */
