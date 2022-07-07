2846a83b77eaa3fd1d1375b2d2e58705f276330c
Evgenii Elchev <yolchev@gmail.com>
2022-07-07 13:36:44 +0700

    guard

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,41 +1,8 @@
-func getDataFromUser(description: String) -> String {
-    print(description)
-    return readLine() ?? ""
+import Darwin
+let a = 2
+
+guard a == 2 else {
+    exit(0)
 }
 
-func showResult(_ result: Int) {
-    print("Результат: " + String(result))
-}
-
-print("Добро пожаловать в программу калькулятор.")
-
-let operation = getDataFromUser(description: "Выберете операцию: +, -, * или /")
-let firstNumber = getDataFromUser(description: "Введите целое число:")
-let secondNumber = getDataFromUser(description: "Введите второе число:")
-
-print("Идет вычисление примера: " + firstNumber + " " + operation + " " + secondNumber)
-
-if let firstNumber = Int(firstNumber) {
-    if let secondNumber = Int(secondNumber) {
-        calculate(operation: operation, firstNumber: firstNumber, secondNumber: secondNumber)
-    } else {
-        print("Вы ввели не верное второе число")
-    }
-} else {
-    print("Вы ввели не верное первое число")
-}
-
-func calculate(operation: String, firstNumber first: Int, secondNumber second: Int) {
-    switch operation {
-    case "+": showResult(first + second)
-    case "-": showResult(first - second)
-    case "*": showResult(first * second)
-    case "/":
-        if second != 0 {
-            showResult(first / second)
-        } else {
-            print("Деление на 0 является недопустимой операцией")
-        }
-    default: print("Вы ввели не верную операцию.")
-    }
-}
+print(a)
