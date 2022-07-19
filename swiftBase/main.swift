2b47e803143c006cbab47ecee9b6e89e8c36737d
Evgenii Elchev <yolchev@gmail.com>
2022-07-19 19:54:44 +0700

    first-look-at-propety-method-and-optional-chaining

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,41 +1,51 @@
-func getDataFromUser(description: String) -> String {
-    print(description)
-    return readLine() ?? ""
+let a: String? = "привет"
+a?.uppercased().first?.uppercased().first?.uppercased()
+/*
+let a: String? = "привет"
+if let newString = a?.uppercased() {
+    print(newString)
 }
-
-func showResult(_ result: Int) {
-    print("Результат: " + String(result))
+ */
+/*
+let a: String? = "привет"
+if let character = a?.first {
+    print(character)
 }
+ */
+/*
+ let a: String? = "привет"
+ if let a = a, let character = a.first {
+     print(character)
+ }
+ */
+/*
+ let a: String? = "привет"
+ if let a = a {
+     a.uppercased()
+ }
+ */
+/*
+let a = 1
+let b = "привет"
+a.isMultiple(of: 2) // false
+b.uppercased() // ПРИВЕТ
+ */
 
-print("Добро пожаловать в программу калькулятор.")
-
-let operation = getDataFromUser(description: "Выберете операцию: +, -, * или /")
-let firstNumber = getDataFromUser(description: "Введите целое число:")
-let secondNumber = getDataFromUser(description: "Введите второе число:")
-
-print("Идет вычисление примера: " + firstNumber + " " + operation + " " + secondNumber)
-
-if let firstNumber = Int(firstNumber) {
-    if let secondNumber = Int(secondNumber) {
-        calculate(operation: operation, firstNumber: firstNumber, secondNumber: secondNumber)
-    } else {
-        print("Вы ввели не верное второе число")
-    }
-} else {
-    print("Вы ввели не верное первое число")
-}
-
-func calculate(operation: String, firstNumber first: Int, secondNumber second: Int) {
-    switch operation {
-    case "+": showResult(first + second)
-    case "-": showResult(first - second)
-    case "*": showResult(first * second)
-    case "/":
-        if second != 0 {
-            showResult(first / second)
-        } else {
-            print("Деление на 0 является недопустимой операцией")
-        }
-    default: print("Вы ввели не верную операцию.")
-    }
-}
+/*
+let a = "привет"
+let b = 1.0
+print(a.count)
+print(b.isZero)
+ */
+/* В консоле мы увидим
+ 6
+ false
+ Program ended with exit code: 0
+ */
+/*
+Int.max     // 9223372036854775807
+*/
+/*
+"привет".count  // 6
+1.isZero        // false
+ */
