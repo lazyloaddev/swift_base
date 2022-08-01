252e0108885cba0f7c53aab0ae1a37ee2dbfe613
Evgenii Elchev <yolchev@gmail.com>
2022-08-01 10:38:03 +0700

    loop

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,132 +1,155 @@
-let a: [Any] = [1, 2, "привет", 3]
-print(a)
+var index = 0
+while index < 10 {
+    print("Текущий индекс " + String(index))
+    index += 1
+    if index == 5 {
+        continue
+    }
+}
 
 /*
-var a = [123, 3, 4, 5, 6]
-a.removeAll()
-print(a)
+ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ for number in numbers {
+     if number == 5 {
+         continue
+     }
+     print(number)
+ }
+ */
+
+/*
+var names = ["иван", "петр", "мария", "кузьма"]
+var hasName = false
+for name in names {
+    if names == "петр" {
+        hasName = true
+        break
+    }
+}
+ */
+
+/*
+ var names = ["иван", "петр", "мария", "кузьма"]
+ var hasName = false
+ for name in names {
+     if names == "петр" {
+         hasName = true
+     }
+ }
+ */
+
+/*
+ var index = 0
+ while index < 10 {
+     break
+     print("Текущий индекс " + String(index))
+     index += 1
+ }
+ */
+/*
+var index = 0
+while index < 10 {
+    print("Текущий индекс " + String(index))
+    index += 1
+    if index > 3 {
+        break
+    }
+}
+*/
+/*
+var index = 20
+repeat {
+    print(index)
+    index += 1
+} while index < 10
 */
 
 /*
- var a = [123, 3, 4, 5, 6]
- a.remove(at: 0)
- print(a)
- a.remove(at: 2)
- print(a)
- */
-/*
- [0, 3, 4, 5, 6]
- Program ended with exit code: 0
- */
-/*
- var a = [123, 3, 4, 5, 6]
- a.insert(0, at: 10)
- print(a)
- */
-/*
-var a = [123, 3, 4, 5, 6]
-a.insert(0, at: 0)
-print(a)
-a.insert(0, at: 3)
-print(a)
- */
-/*
- var a = [123, 3, 4, 5, 6]
- a.append(0)
- print(a)
+ var summ = 0
+ var step = 0
+ while summ < 1000 {
+     summ += Int.random(in: 0...100)
+     step += 1
+ }
+ print(summ, step)
  */
 
 /*
-let a = [123, 3, 4, 5, 6]
-if a.count > 5 {
-    print(a[5])
-} else {
-    print("нет такого значения")
+ var index = 0
+ while index < 10 {
+     print("Текущий индекс " + String(index))
+ }
+ */
+
+/*
+var index = 0
+while index < 10 {
+    print("Текущий индекс " + String(index))
+    index += 1
+}
+*/
+/*
+for element in stride(from: 0, to: 10, by: 2) {
+    print("привет " + String(element))
+}
+*/
+/*
+ for _ in 0..<5 {
+     print("привет")
+ }
+ */
+/*
+for element in 0..<5 {
+    print("привет " + String(element))
 }
  */
 /*
- let a = [123, 3, 4, 5, 6]
- print(a[5])
+ for element in 1...5 {
+     print("привет")
+ }
  */
 /*
- let a = [123, 3, 4, 5, 6]
+ let fiveTimes = [1, 2, 3, 4, 5]
+ for element in fiveTimes {
+     print("привет")
+ }
  */
-/* В консоле мы увидим
- 123 3 4 5 6
- Program ended with exit code: 0
+
+/*
+ let salary = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
+ var summ = 0
+ for element in salary {
+     summ += element
+ }
+ print(summ)
  */
+
 /*
-let a = [
-    123,    // 0
-    3,      // 1
-    4,      // 2
-    5,      // 3
-    6       // 4
-]
+ let salary = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
+ for element in salary {
+     print(element)
+ }
  */
+
 /*
-var b = [1, 2, 3, 4, 5]
-b = [1, 2, 3, 4, 5]
- */
-/*
- var a = 2
- a = 10
- var b = [1, 2, 3, 4, 5]
- b = [6, 7, 8, 9, 10]
- */
-/*
-let names: [String]? = nil
- */
-/*
-let names = [] // так нельзя, компилятор не понимает какой тип будет храниться
-// нужно
-let names = [String]()
-// или
-let names:[String] = []
- */
-/*
-let names = ["Иван", "Петр", "Виктория"]
-*/
-/*
-let names: [String] = ["Иван", "Петр", "Виктория"]
- */
-/*
-let names: Array<String> = Array<String>(arrayLiteral: "Иван", "Петр", "Виктория")
- */
-/*
-let age = 25
-let name = "Иван"
-let salary: Array<Int> = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
-let childenNames: Array<String> = ["Егор", "Екатерина", "Василиса"]
-*/
-/*
-let age = 25
-let name = "Иван"
 let salary = [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012]
-let childenNames = ["Егор", "Екатерина", "Василиса"]
+let summ = salary[0]
+    + salary[1]
+    + salary[2]
+    + salary[3]
+    + salary[4]
+    + salary[5]
+    + salary[6]
+    + salary[7]
+    + salary[8]
+    + salary[9]
+    + salary[10]
+    + salary[11]
 */
 /*
-let age = 25
-let name = "Иван"
-let salary1 = 50001
-let salary2 = 47002
-let salary3 = 53003
-let salary4 = 51004
-let salary5 = 52005
-let salary6 = 55006
-let salary7 = 59007
-let salary8 = 56008
-let salary9 = 58009
-let salary10 = 57010
-let salary11 = 50011
-let salary12 = 50012
-let child1Name = "Егор"
-let child2Name = "Екатерина"
-let child3Name = "Василиса"
-*/
-/*
-let age = 25
-let name = "Иван"
-let salary = 50000
-let childName = "Егор"
-*/
+ print("Привет")
+ print("Привет")
+ print("Привет")
+ print("Привет")
+ print("Привет")
+ */
