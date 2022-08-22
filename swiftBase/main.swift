861afdaeb64e0a6e3a6be59db75b65e2c7c734c5
Evgenii Elchev <yolchev@gmail.com>
2022-08-22 16:11:55 +0700

    30-creating-string

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,35 +1,7 @@
+
 let string = """
     привет
-        "мир"
-    и пока
+    мир
     """
+
 print(string)
-
-/*
- let string = """
-     привет
-     "мир"
-     """
- print(string)
- */
-
-/*
- let string = """
-     привет
-     мир
-     """
- print(string)
- */
-
-/*
- let string = "привет \"мир\""
- print(string)
- */
-
-/*
- let a = 45
- let b = 34.423423423423423
- let string = "a: \(a) b: \(b) summ: \(a + 98)"
- print(string)
-
- */
