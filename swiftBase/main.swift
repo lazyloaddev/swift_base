f4c0dbb6aa53e57809ef704f623b69adaa6a1d76
Evgenii Elchev <yolchev@gmail.com>
2022-08-22 13:10:47 +0700

    creating string

--- swiftBase/main.swift
+++ swiftBase/main.swift
@@ -1,63 +1,35 @@
-func sum(_ a: Int, _ b: Int) -> Int {
-    return a + b
-}
-
-func minus(_ a: Int, _ b: Int) -> Int {
-    a - b
-}
-
-func sum(a: Int, andB b: Int, andMinus c: Int) -> Int {
-    minus(sum(a, b), c)
-}
-
-func calc(_ a: Int, _ b: Int, _ c: Int) {
-    let result = sum(a: a, andB: b, andMinus: c)
-    print(result)
-}
-
-calc(1, 2, 3)
+let string = """
+    привет
+        "мир"
+    и пока
+    """
+print(string)
 
 /*
-for i in 0..<100 {
-    print(i)
-}
-*/
-/*
-func sum(_ a: Int, _ b: Int) -> Int {
-    a + b
-}
-
-func minus(_ a: Int, _ b: Int) -> Int {
-    a - b
-}
-
-func sum(a: Int, andB b: Int, andMinus c: Int) -> Int {
-    let summResult = sum(a, b)
-    let minusResult = minus(summResult, c)
-    return minusResult
-}
-
-func calc(_ a: Int, _ b: Int, _ c: Int) {
-    let result = sum(a: a, andB: b, andMinus: c)
-    print(result)
-}
-
-calc(1, 2, 3)
-*/
-
-
+ let string = """
+     привет
+     "мир"
+     """
+ print(string)
+ */
 
 /*
-var a = [1, 2, 3, 4, 5]
-if a.count > 5 {
-    a.append(1)
-} else {
-    a.append(9)
-}
+ let string = """
+     привет
+     мир
+     """
+ print(string)
+ */
 
-if a.count > 5 {
-    a.append(1)
-} else {
-    a.append(9)
-}
-*/
+/*
+ let string = "привет \"мир\""
+ print(string)
+ */
+
+/*
+ let a = 45
+ let b = 34.423423423423423
+ let string = "a: \(a) b: \(b) summ: \(a + 98)"
+ print(string)
+
+ */
