b6f2417dc83dc78cf82d5b3bf536a4893672f6a4
Evgenii Elchev <yolchev@gmail.com>
2022-09-29 15:06:51 +0700

    class basic

--- swiftBase/Application.swift
+++ swiftBase/Application.swift
@@ -1,388 +1,227 @@
-@main
-struct Application {
-    static func main() throws {
-        let person = Person(
-            height: 180,
-            age: 25,
-            surname: "Иванов",
-            name: "Иван",
-            patronymic: "Иванович",
-            salary: [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012],
-            children: ["Егор", "Екатерина", "Василиса"]
-        )
-        person.add(salary: 50013)
-        person.printToConsole()
-    }
-}
-
 class Person {
-    let height: Int
-    let age: Int
+
     let surname: String
     let name: String
-    let patronymic: String
-    var salary: [Int]
-    let children: [String]
+    let patronymic: String?
     
-    init(
-        height: Int,
-        age: Int,
-        surname: String,
-        name: String,
-        patronymic: String,
-        salary: [Int],
-        children: [String]
-    ) {
-        self.height = height
-        self.age = age
+    init(surname: String, name: String, patronymic: String?) {
         self.surname = surname
         self.name = name
         self.patronymic = patronymic
-        self.salary = salary
-        self.children = children
     }
     
     func getFullName() -> String {
-        return "\(self.name) \(self.surname) \(self.patronymic)"
-    }
-
-    func getFullSalary() -> Int {
-        var fullSalary = 0
-        for summ in self.salary {
-            fullSalary += summ
-        }
-        return fullSalary
-    }
-
-    func getAverageSalary() -> Int {
-        let fullSalary = getFullSalary()
-        let averageSalary = fullSalary / self.salary.count
-        return averageSalary
-    }
-
-    func add(salary: Int) {
-        self.salary.append(50013)
-    }
-
-    func printToConsole() {
-        let fullName = getFullName()
-        let fullSalary = getFullSalary()
-        let averageSalary = getAverageSalary()
-        
-        print("Имя сотрудника: \(fullName)")
-        print("Полная сумма выплат: \(fullSalary)")
-        print("Средняя зарплата: \(averageSalary)")
+        return "\(name) \(surname) \(patronymic ?? "")"
     }
     
 }
 
-/*
- @main
- struct Application {
-     static func main() throws {
-         let person = Person(
-             height: 180,
-             age: 25,
-             surname: "Иванов",
-             name: "Иван",
-             patronymic: "Иванович",
-             salary: [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012],
-             children: ["Егор", "Екатерина", "Василиса"]
-         )
-         person.add(salary: 50013)
-         person.printToConsole()
-     }
- }
 
+@main
+struct Application {
+    static func main() throws {
+        let person1 = Person(surname: "Иванов", name: "Иван", patronymic: "Иванович")
+        print(person1.getFullName())
+        let person2 = Person(surname: "Кузьмин", name: "Кузьма", patronymic: "Кузьмич")
+        print(person2.getFullName())
+    }
+}
+
+/*
  class Person {
-     let height: Int
-     let age: Int
-     let weight: Int
+
      let surname: String
      let name: String
-     let patronymic: String
-     var salary: [Int]
-     let children: [String]
+     let patronymic: String?
      
-     init(
-         height: Int,
-         age: Int,
-         weight: Int,
-         surname: String,
-         name: String,
-         patronymic: String,
-         salary: [Int],
-         children: [String]
-     ) {
-         self.height = height
-         self.age = age
+     init(surname: String, name: String, patronymic: String?) {
          self.surname = surname
          self.name = name
          self.patronymic = patronymic
-         self.salary = salary
-         self.children = children
      }
      
      func getFullName() -> String {
-         return "\(self.name) \(self.surname) \(self.patronymic)"
-     }
-
-     func getFullSalary() -> Int {
-         var fullSalary = 0
-         for summ in self.salary {
-             fullSalary += summ
-         }
-         return fullSalary
-     }
-
-     func getAverageSalary() -> Int {
-         let fullSalary = getFullSalary()
-         let averageSalary = fullSalary / self.salary.count
-         return averageSalary
-     }
-
-     func add(salary: Int) {
-         self.salary.append(50013)
-     }
-
-     func printToConsole() {
-         let fullName = getFullName()
-         let fullSalary = getFullSalary()
-         let averageSalary = getAverageSalary()
-         
-         print("Имя сотрудника: \(fullName)")
-         print("Полная сумма выплат: \(fullSalary)")
-         print("Средняя зарплата: \(averageSalary)")
+         return "\(self.name) \(self.surname) \(self.patronymic ?? "")"
      }
      
  }
  */
 
 /*
- @main
- struct Application {
-     static func main() throws {
-         let person = Person(
-             height: 180,
-             age: 25,
-             surname: "Иванов",
-             name: "Иван",
-             patronymic: "Иванович",
-             salary: [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012],
-             children: ["Егор", "Екатерина", "Василиса"]
-         )
-         person.add(salary: 50013)
-         person.printToConsole()
-     }
- }
-
  class Person {
-     let height: Int
-     let age: Int
-     let surname: String
-     let name: String
-     let patronymic: String
-     var salary: [Int]
-     let children: [String]
+
+     let surname
+     let name
+     let patronymic: String?
      
-     init(
-         height: Int,
-         age: Int,
-         surname: String,
-         name: String,
-         patronymic: String,
-         salary: [Int],
-         children: [String]
-     ) {
-         self.height = height
-         self.age = age
-         self.surname = surname
-         self.name = name
-         self.patronymic = patronymic
-         self.salary = salary
-         self.children = children
-     }
-     
-     func getFullName() -> String {
-         return "\(self.name) \(self.surname) \(self.patronymic)"
-     }
-
-     func getFullSalary() -> Int {
-         var fullSalary = 0
-         for summ in self.salary {
-             fullSalary += summ
-         }
-         return fullSalary
-     }
-
-     func getAverageSalary() -> Int {
-         let fullSalary = getFullSalary()
-         let averageSalary = fullSalary / self.salary.count
-         return averageSalary
-     }
-
-     func add(salary: Int) {
-         self.salary.append(50013)
-     }
-
-     func printToConsole() {
-         let fullName = getFullName()
-         let fullSalary = getFullSalary()
-         let averageSalary = getAverageSalary()
+     init() {
          
-         print("Имя сотрудника: \(fullName)")
-         print("Полная сумма выплат: \(fullSalary)")
-         print("Средняя зарплата: \(averageSalary)")
      }
      
  }
  */
 
+/*
+ class Person {
+
+     let surname = ""
+     let name = ""
+     let patronymic: String?
+     
+     init() {
+         
+     }
+     
+ }
+ */
 
 /*
+ class Person {
+
+     var surname = ""
+     var name = ""
+     var patronymic: String?
+     
+     init() {
+         
+     }
+     
+ }
+ */
+
+/*
+ class Person {
+
+     var surname = ""
+     var name = ""
+     var patronymic = ""
+     
+ }
+
+
  @main
  struct Application {
      static func main() throws {
-         var person = (
-             height: 180,
-             age: 25,
-             surname: "Иванов",
-             name: "Иван",
-             patronymic: "Иванович",
-             salary: [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012],
-             children: ["Егор", "Екатерина", "Василиса"]
-         )
-         person = add(salary: 50013, toPerson: person)
-         printToConsole(person: person)
+         let person1 = Person()
+         person1.surname = "Иванов"
+         person1.name = "Иван"
+         person1.patronymic = "Иванович"
+         print(person1.surname, person1.name, person1.patronymic!)
+         let person2 = Person()
+         person2.surname = "Кузьмин"
+         person2.name = "Кузьма"
+         person2.patronymic = "Кузьмич"
+         print(person2.surname, person2.name, person2.patronymic!)
      }
  }
+ */
 
- typealias Person = (
-     height: Int,
-     age: Int,
-     surname: String,
-     name: String,
-     patronymic: String,
-     salary: [Int],
-     children: [String]
- )
+/*
+ class Person {
 
- func getFullName(person: Person) -> String {
-     return "\(person.name) \(person.surname) \(person.patronymic)"
- }
-
- func getFullSalary(person: Person) -> Int {
-     var fullSalary = 0
-     for summ in person.salary {
-         fullSalary += summ
-     }
-     return fullSalary
- }
-
- func getAverageSalary(person: Person) -> Int {
-     let fullSalary = getFullSalary(person: person)
-     let averageSalary = fullSalary / person.salary.count
-     return averageSalary
- }
-
- func add(salary: Int, toPerson person: Person) -> Person {
-     var person = person
-     person.salary.append(50013)
-     return person
- }
-
- func printToConsole(person: Person) {
-     let fullName = getFullName(person: person)
-     let fullSalary = getFullSalary(person: person)
-     let averageSalary = getAverageSalary(person: person)
+     var surname = "Иванов"
+     var name = "Иван"
+     var patronymic = "Иванович"
      
-     print("Имя сотрудника: \(fullName)")
-     print("Полная сумма выплат: \(fullSalary)")
-     print("Средняя зарплата: \(averageSalary)")
- }
- */
-
-/*
- var car = (
-     260,
-     300,
-     "Tesla",
-     "Model 3",
-     "Sedan",
-     [270, 270, 260, 260],
-     ["Elon", "Steve", "John"]
- )
- getFullName(person: car)
- */
-
-/*
- var person: Person = (
-     height: 180,
-     weight: 70,
-     age: 25,
-     surname: "Иванов",
-     name: "Иван",
-     patronymic: "Иванович",
-     salary: [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012],
-     children: ["Егор", "Екатерина", "Василиса"]
- )
- person = add(salary: 50013, toPerson: person)
- printToConsole(person: person)
- */
-
-/*
- var person = (
-     height: 180,
-     weight: 70,
-     age: 25,
-     surname: "Иванов",
-     name: "Иван",
-     patronymic: "Иванович",
-     salary: [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012],
-     children: ["Егор", "Екатерина", "Василиса"]
- )
- person = add(salary: 50013, toPerson: person)
- printToConsole(person: person)
- */
-
-/*
- func getFullName(person: (height: Int, age: Int, surname: String, name: String, patronymic: String, salary: [Int], children: [String])) -> String {
-     return "\(person.name) \(person.surname) \(person.patronymic)"
  }
 
- func getFullSalary(person: (height: Int, age: Int, surname: String, name: String, patronymic: String, salary: [Int], children: [String])) -> Int {
-     var fullSalary = 0
-     for summ in person.salary {
-         fullSalary += summ
+
+ @main
+ struct Application {
+     static func main() throws {
+         let person1 = Person()
+         print(person1.surname, person1.name, person1.patronymic)
+         let person2 = Person()
+         person2.surname = "Кузьмин"
+         person2.name = "Кузьма"
+         person2.patronymic = "Кузьмич"
+         print(person2.surname, person2.name, person2.patronymic)
      }
-     return fullSalary
  }
  */
 
+/*
+ class Person {
+
+     let surname = "Иванов"
+     let name = "Иван"
+     let patronymic = "Иванович"
+     
+ }
+
+
+ @main
+ struct Application {
+     static func main() throws {
+         let person1 = Person()
+         print(person1.surname, person1.name, person1.patronymic)
+         let person2 = Person()
+         print(person2.surname, person2.name, person2.patronymic)
+     }
+ }
+ */
+/*
+ class Person {
+
+     let surname = "Иванов"
+     let name = "Иван"
+     let patronymic = "Иванович"
+     
+ }
+
+
+ @main
+ struct Application {
+     static func main() throws {
+         let person = Person()
+         print(person.surname, person.name, person.patronymic)
+     }
+ }
+ */
 
 /*
- var person = (
-     height: 180,
-     age: 25,
-     surname: "Иванов",
-     name: "Иван",
-     patronymic: "Иванович",
-     salary: [50001, 47002, 53003, 51004, 52005, 55006, 59007, 56008, 58009, 57010, 50011, 50012],
-     children: ["Егор", "Екатерина", "Василиса"]
- )
- // 2
- let fullName = "\(person.name) \(person.surname) \(person.patronymic)"
- // 3
- var fullSalary = 0
- for summ in person.salary {
-     fullSalary += summ
+ class Person {}
+
+
+ @main
+ struct Application {
+     static func main() throws {
+         let person: Person = Person()
+         print(person)
+     }
  }
- // 4
- let averageSalary = fullSalary / person.salary.count
- // 5
- person.salary.append(50013)
- // 6
- print("Имя сотрудника: \(fullName)")
- print("Полная сумма выплат: \(fullSalary)")
- print("Средняя зарплата: \(averageSalary)")
+ */
+/*
+ class Person {}
+
+ @main
+ struct Application {
+     static func main() throws {
+         print(Person())
+     }
+ }
+ */
+/*
+ class Person {} // Класс
+
+
+ @main
+ struct Application {
+     static func main() throws {
+         Person()        // Создаем экземпляр данного класса
+     }
+ }
+ */
+/*
+ class Person {}     // Хорошее имя
+ class B {}          // Плохое имя
+ class 1Person {}    // Некоректное имя, приложение не скомпилиируется
+ class person {}     // Плохое имя, выглдяит как переменная, а не тип
+ class Myperson {}   // Плохое имя, слова сливаются
+ class My_person {}  // Плохое имя, в iOS разрабоке обычно не используют нижнее подчеркивание для разделения слов
+ class MyPerson {}   // Хорошее имя
+ */
+/*
+ class Person {
+ }
  */
