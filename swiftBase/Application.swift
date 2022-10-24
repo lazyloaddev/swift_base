7fcbfdbad2286d4403269b962a7b0cf73467a7d8
Evgenii Elchev <yolchev@gmail.com>
2022-10-24 15:09:17 +0700

    48-basic-memory-maangment

--- swiftBase/Application.swift
+++ swiftBase/Application.swift
@@ -1,189 +1,25 @@
-class Passport {
-    var number: String
-    init(number: String) {
-        self.number = number
-    }
-}
-
-struct Person {
-    var name: String
-    var passport: Passport
-    
-    init(name: String, passport: Passport) {
-        self.name = name
-        self.passport = passport
-    }
-}
-
+// константа v объявлена в глобальной области видимости
+// будет создаваться при запуске программы
+// и удалиться только при завершении программы
+let v = 4
 
 @main
 struct Application {
     static func main() throws {
-        var a = Person(
-            name: "Иван",
-            passport: Passport(number: "01 02 334455")
-        )
-        var b = a
-        a.name = "Кузьма"
-        a.passport.number = "00 00 000000"
-        print(a.name, a.passport.number)
-        print(b.name, b.passport.number)
+        // в начале каждой итерации будет создаваться новая константа i
+        for i in 0...15 {
+            // на каждой итерации будет создаваться новая константа result
+            let result = summ(a: i, b: v)
+            print(result)
+            // в конце каждой итерации константы i и result будут удаляться
+        }
     }
 }
 
-/*
- class A {
-     var name: String
-     init(name: String) {
-         self.name = name
-     }
-     
-     func copy() -> A {
-         return A(name: name)
-     }
- }
-
-
- @main
- struct Application {
-     static func main() throws {
-         var a = A(name: "Иван")
-         var b = a.copy()
-         print(a, b)
-     }
- }
- */
-
-/*
- class A {
-     var name: String
-     init(name: String) {
-         self.name = name
-     }
- }
-
-
- @main
- struct Application {
-     static func main() throws {
-         var a = A(name: "Иван")
-         var b = A(name: a.name)
-     }
- }
- */
-
-/*
- class A {
-     var name: String
-     init(name: String) {
-         self.name = name
-     }
- }
-
- struct B {
-     var name: String
-     init(name: String) {
-         self.name = name
-     }
- }
-
- @main
- struct Application {
-     static func main() throws {
-         var a = A(name: "Иван")
-         var b = a
-         b.name = "Кузьма"
-         var c = A(name: "Петр")
-         
-         var d = B(name: "Иван")
-         var e = d
-         d.name = "Кузьма"
-         print(a.name, b.name, c.name, d.name)
-     }
- }
- */
-
-/*
- class A {
-     var name: String
-     init(name: String) {
-         self.name = name
-     }
- }
-
- @main
- struct Application {
-     static func main() throws {
-         // 1
-         var a = A(name: "Иван")
-         // 2
-         var b = A(name: "Иван")
-         // 3
-         var c = A(name: "Иван")
-         // 4
-         b.name = "Кузьма"
-         // 5
-         a.name = "Петр"
-         // 6
-         var d = A(name: "Иван")
-         // 6
-         print(a.name, b.name, c.name, d.name)
-     }
- }
- */
-/*
- class A {
-     var name: String
-     init(name: String) {
-         self.name = name
-     }
- }
-
- @main
- struct Application {
-     static func main() throws {
-         // 1
-         var a = A(name: "Иван")
-         // 2
-         var b = a
-         // 3
-         var c = b
-         // 4
-         b.name = "Кузьма"
-         // 5
-         a.name = "Петр"
-         // 6
-         var d = b
-         print(a.name, b.name, c.name, d.name)
-     }
- }
- */
-
-/*
- struct A {
-     var name: String
-     init(name: String) {
-         self.name = name
-     }
- }
-
- @main
- struct Application {
-     static func main() throws {
-         // 1
-         var a = A(name: "Иван")
-         // 2
-         var b = a
-         // 3
-         var c = b
-         // 4
-         b.name = "Кузьма"
-         // 5
-         a.name = "Петр"
-         // 6
-         var d = b
-         print(a, b, c, d)
-     }
- }
-
- */
+// при каждом вызове функции будут создаваться новые константы a и b
+func summ(a: Int, b: Int) -> Int {
+    // при каждом вызове функции будет создаваться новая константа result
+    let result = a + b
+    return result
+    // в конце каждого вызова константы a, b и result будут удаляться
+}
