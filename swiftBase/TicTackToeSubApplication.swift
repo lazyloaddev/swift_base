7cb533c34059d2fd72f9c17403f8754243c67eb2
Evgenii Elchev <yolchev@gmail.com>
2022-11-07 13:31:31 +0700

    52-clia-better-ttt-classes

--- swiftBase/TicTackToeSubApplication.swift
+++ swiftBase/TicTackToeSubApplication.swift
@@ -1,12 +1,7 @@
 import Foundation
 
 class TicTackToeSubApplication: SubApplication {
-    private let symbols = (
-        empty: " ",
-        player1: "X",
-        player2: "O"
-    )
-    private var field = [[String]]()
+    private var field = TicTackToeField(size: 0)
     
     init() {
         super.init(
@@ -22,54 +17,49 @@ class TicTackToeSubApplication: SubApplication {
             print("Вы ввели неверное имя первого игрока")
             return .resume
         }
+        let player1 = TicTackToePlayer(name: player1Name, symbol: .x)
         let player2Name = UserDataProvider.string("Введите имя второго игрока")
         guard player2Name.count != 0 else {
             print("Вы ввели неверное имя второго игрока")
             return .resume
         }
+        let player2 = TicTackToePlayer(name: player2Name, symbol: .y)
         let fieldSize = UserDataProvider.int("Введите размер игрового поля")
         guard fieldSize > 0 else {
             print("Вы ввели неверный размер игрового поля")
             return .resume
         }
         
-        field = [[String]]()
-        for _ in 0..<fieldSize {
-            var row = [String]()
-            for _ in 0..<fieldSize {
-                row.append(symbols.empty)
-            }
-            field.append(row)
-        }
-        printField()
+        field = TicTackToeField(size: fieldSize)
+        field.printToConsole()
         
-        var winnerSymbol: String?
+        var winnerSymbol: TicTackToeFieldSymbol?
         while true {
-            playerTurn(playerName: player1Name, symbol: symbols.player1)
-            printField()
-            if let symbol = checkPlayerWon() {
+            playerTurn(player: player1)
+            field.printToConsole()
+            if let symbol = field.checkFilled() {
                 winnerSymbol = symbol
                 break
             }
-            if checkIfGameOver() {
+            if field.isAllTurnsDone {
                 break
             }
             
-            playerTurn(playerName: player2Name, symbol: symbols.player2)
-            printField()
-            if let symbol = checkPlayerWon() {
+            playerTurn(player: player2)
+            field.printToConsole()
+            if let symbol = field.checkFilled() {
                 winnerSymbol = symbol
                 break
             }
-            if checkIfGameOver() {
+            if field.isAllTurnsDone {
                 break
             }
         }
         
-        if winnerSymbol == symbols.player1 {
-            print("Победил: \(player1Name)")
-        } else if winnerSymbol == symbols.player2 {
-            print("Победил: \(player2Name)")
+        if winnerSymbol == player1.symbol {
+            print("Победил: \(player1.name)")
+        } else if winnerSymbol == player2.symbol {
+            print("Победил: \(player2.name)")
         } else {
             print("Игра окончилась в ничью")
         }
@@ -78,20 +68,115 @@ class TicTackToeSubApplication: SubApplication {
         return shouldStartNewGame == "да" ? .resume : .exit
     }
     
-    private func checkPlayerWon() -> String? {
-        return checkPlayerWonByRows()
-        ?? checkPlayerWonByColumn()
-        ?? checkPlayerWonByFirstDiagonal()
-        ?? checkPlayerWonBySecondDiagonal()
+    private func playerTurn(player: TicTackToePlayer) {
+        while true {
+            print("\(player.name) делает ход:")
+            let row = UserDataProvider.int("Введите номер строки")
+            let column = UserDataProvider.int("Введите номер столбца")
+            let result = field.set(symbol: player.symbol, row: row, column: column)
+            switch result {
+            case.incorrectRow:
+                print("Введен неверный номер строки")
+            case .incorrectColumn:
+                print("Введен неверный номер столбца")
+            case .turnAlreadyComplted:
+                print("Такой ход уже был")
+            case .success:
+                return
+            }
+        }
     }
     
-    private func checkPlayerWonBySecondDiagonal() -> String? {
+}
+
+private enum TicTackToeFieldSymbol: String {
+    case empty = " "
+    case x = "X"
+    case y = "Y"
+}
+
+private enum TicTackToeFieldSetResult {
+    case incorrectRow
+    case incorrectColumn
+    case turnAlreadyComplted
+    case success
+}
+
+private struct TicTackToePlayer {
+    let name: String
+    let symbol: TicTackToeFieldSymbol
+}
+
+private struct TicTackToeField {
+    private var field: [[TicTackToeFieldSymbol]]
+    
+    init(size: Int) {
+        field = [[TicTackToeFieldSymbol]]()
+        for _ in 0..<size {
+            var row = [TicTackToeFieldSymbol]()
+            for _ in 0..<size {
+                row.append(.empty)
+            }
+            field.append(row)
+        }
+    }
+    
+    func printToConsole() {
+        let fieldSize = field.count
+        var fieldForamtedString = ""
+        for row in field {
+            fieldForamtedString += "|"
+            for cell in row {
+                fieldForamtedString += "\(cell.rawValue)|"
+            }
+            fieldForamtedString += "\n"
+            for _ in 0..<(fieldSize * 2 + 1) {
+                fieldForamtedString += "-"
+            }
+            fieldForamtedString += "\n"
+        }
+        print(fieldForamtedString)
+    }
+    
+    subscript(row: Int, column: Int) -> TicTackToeFieldSymbol? {
+        guard
+            row > 0, row <= field.count,
+            column > 0, column <= field.count
+        else { return nil }
+        
+        let rowInArray = row - 1
+        let columnInArray = column - 1
+        
+        return field[rowInArray][columnInArray]
+    }
+    
+    mutating func set(symbol: TicTackToeFieldSymbol, row: Int, column: Int) -> TicTackToeFieldSetResult {
+        guard row > 0, row <= field.count else { return .incorrectRow }
+        guard column > 0, column <= field.count else { return .incorrectColumn }
+        let rowInArray = row - 1
+        let columnInArray = column - 1
+        
+        guard field[rowInArray][columnInArray] == .empty else {
+            return .turnAlreadyComplted
+        }
+        
+        field[rowInArray][columnInArray] = symbol
+        
+        return .success
+    }
+    
+    func checkFilled() -> TicTackToeFieldSymbol? {
+        return checkRowsFilled()
+            ?? checkColumnFilled()
+            ?? checkFirstDiagonalFilled()
+            ?? checkSecondDiagonalFilled()
+    }
+    
+    private func checkSecondDiagonalFilled() -> TicTackToeFieldSymbol? {
         let fieldSize = field.count
         let lastIndex = fieldSize - 1
         let firstSymbol = field[0][lastIndex]
-        guard firstSymbol != symbols.empty else {
-            return nil
-        }
+        guard firstSymbol != .empty else { return nil }
         var isWin = true
         for i in 0..<fieldSize {
             if field[i][lastIndex - i] != firstSymbol {
@@ -106,11 +191,11 @@ class TicTackToeSubApplication: SubApplication {
         return nil
     }
     
-    private func checkPlayerWonByFirstDiagonal() -> String? {
+    private func checkFirstDiagonalFilled() -> TicTackToeFieldSymbol? {
         let fieldSize = field.count
         let firstSymbol = field[0][0]
         
-        guard firstSymbol != symbols.empty else {
+        guard firstSymbol != .empty else {
             return nil
         }
         
@@ -128,12 +213,12 @@ class TicTackToeSubApplication: SubApplication {
         return nil
     }
     
-    private func checkPlayerWonByColumn() -> String? {
+    private func checkColumnFilled() -> TicTackToeFieldSymbol? {
         let fieldSize = field.count
         
         for i in 0..<fieldSize {
             let firstSymbol = field[0][i]
-            if firstSymbol == symbols.empty {
+            if firstSymbol == .empty {
                 break
             }
             var isWin = true
@@ -150,12 +235,12 @@ class TicTackToeSubApplication: SubApplication {
         return nil
     }
     
-    private func checkPlayerWonByRows() -> String? {
+    private func checkRowsFilled() -> TicTackToeFieldSymbol? {
         let fieldSize = field.count
         
         for i in 0..<fieldSize {
             let firstSymbol = field[i][0]
-            if firstSymbol == symbols.empty {
+            if firstSymbol == .empty {
                 break
             }
             var isWin = true
@@ -172,58 +257,15 @@ class TicTackToeSubApplication: SubApplication {
         return nil
     }
     
-    private func checkIfGameOver() -> Bool {
+    var isAllTurnsDone: Bool {
         for row in field {
             for cell in row {
-                if cell == symbols.empty {
+                if cell == .empty {
                     return false
                 }
             }
         }
         return true
-    }
-    
-    private func playerTurn(playerName: String, symbol: String) {
-        let fieldSize = field.count
-        while true {
-            print("\(playerName) делает ход:")
-            let row = UserDataProvider.int("Введите номер строки")
-            guard row > 0, row <= fieldSize else {
-                print("Введен неверный номер строки")
-                continue
-            }
-            let column = UserDataProvider.int("Введите номер столбца")
-            guard column > 0, column <= fieldSize else {
-                print("Введен неверный номер столбца")
-                continue
-            }
-            let rowInArray = row - 1
-            let columnInArray = column - 1
-            
-            guard field[rowInArray][columnInArray] == symbols.empty else {
-                print("Такой ход уже был")
-                continue
-            }
-            field[rowInArray][columnInArray] = symbol
-            break
-        }
-    }
-    
-    private func printField() {
-        let fieldSize = field.count
-        var fieldForamtedString = ""
-        for row in field {
-            fieldForamtedString += "|"
-            for cell in row {
-                fieldForamtedString += "\(cell)|"
-            }
-            fieldForamtedString += "\n"
-            for _ in 0..<(fieldSize * 2 + 1) {
-                fieldForamtedString += "-"
-            }
-            fieldForamtedString += "\n"
-        }
-        print(fieldForamtedString)
     }
     
 }
